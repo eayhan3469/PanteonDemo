0413a5d8861289eca9bd44b0f54204339c5e8e09
Egemen Ayhan <eayhan3469@gmail.com>
2021-02-11 22:43:20 +0300

    gitIgnore file fixed.

---
+++ PanteonDemo/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    private bool _dragging;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            _dragging = true;
+        else if (Input.GetMouseButtonUp(0))
+            _dragging = false;
+
+        if (_dragging)
+        {
+            _rigidBody.AddForce(Vector3.forward, ForceMode.Acceleration);
+        }
+    }
+}
